74cd979550fc519f521a0858187a8767be8e0877
cheng. chi <44891397+chichengcn@users.noreply.github.com>
2019-06-14 21:21:10 +0800

    upload
    gnc01

---
+++ com_ground/src/serial_node.h
@@ -0,0 +1,13 @@
+
+#ifndef SERIAL_NODE_H
+#define SERIAL_NODE_H
+
+// ROS includes
+#include <ros/ros.h>
+#include <geometry_msgs/QuaternionStamped.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <std_msgs/UInt8.h>
+
+
+#endif // SERIAL_NODE_H
